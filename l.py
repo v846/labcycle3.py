8c2674f9e1d3d5c71a0806cb5e3c991cabd61888
v846 <79577421+v846@users.noreply.github.com>
2021-09-18 16:35:48 +0530

    Add files via upload

---
+++ l.py
@@ -0,0 +1,8 @@
+def check(string, sub_str):
+    if (string.find(sub_str) == -1):
+        print("NO")
+    else:
+        print("YES")
+string = "geeks for geeks"
+sub_str ="geek"
+check(string, sub_str)
